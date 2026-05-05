1bf8b57c6ad944d7fbff6ca6d18704bab78d9030
Swayam <194781119+Swayam8867@users.noreply.github.com>
2026-05-05 22:57:47 +0530

    Add files via upload

---
+++ task 1/script.js
@@ -0,0 +1,23 @@
+function scrollToJoin() {
+    document.getElementById("join").scrollIntoView({ behavior: "smooth" });
+}
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    const update = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+
+        const increment = target / 100;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(update, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    update();
+});
